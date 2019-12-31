a05990447ab1fb574097db355ae25f484133ba85
Mario Jr <juniorm3@msn.com>
2019-12-31 15:47:25 -0300

    Validações com Bean Validation

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,37 +19,42 @@ import com.algaworks.cobranca.model.Titulo;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv =  new ModelAndView("CadastroTitulo");
-		return mv;
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {	
-		titulos.save(titulo);
-		
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem","Título salvo com sucesso!");
+		mv.addObject(new Titulo());
 		return mv;
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+
+		if(errors.hasErrors()) {
+			return mv;
+		}
+		
+		titulos.save(titulo);
+
+		mv.addObject("mensagem", "Título salvo com sucesso!");
+		return mv;
+	}
+
 	@RequestMapping
 	public ModelAndView pesquisar() {
 		List<Titulo> todostitulos = titulos.findAll();
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todostitulos);
-		
+
 		return mv;
 	}
-	
-	
+
 	@ModelAttribute("todosStatustitulo")
-	public List<StatusTitulo> todosStatusTitulo(){
+	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
 }
